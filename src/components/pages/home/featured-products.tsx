04e5bb5e3bee138a5ef463d7a535669bb1cbf78a
skupta <tosya2001@gmail.com>
2025-04-26 13:57:04 +0300

    --

--- src/components/pages/home/featured-products.tsx
+++ src/components/pages/home/featured-products.tsx
@@ -15,7 +15,7 @@ export const FeaturedProducts: React.FC<FeaturedProductsProps> = async ({
   className,
 }) => {
   const products = await getCollectionProducts({
-    collection: "hidden-featured",
+    collection: "women",
   });
 
   return (
