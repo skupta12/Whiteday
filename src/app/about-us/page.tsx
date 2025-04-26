0af7880590aefa533086d76c1a34a8886777b14d
skupta <tosya2001@gmail.com>
2025-04-26 13:44:57 +0300

    small fixes

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -1,10 +1,12 @@
 import { Care, Hero } from "@/components/pages/about";
+import { Insta } from "@/components/pages/home";
 
 export default function AboutPage() {
   return (
     <>
       <Hero />
       <Care />
+      <Insta />
     </>
   );
 }
