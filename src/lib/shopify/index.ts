e41c7deb3577d1a3ad15f0f08858584668fefeb8
skupta <tosya2001@gmail.com>
2025-04-30 15:14:35 +0300

    --

--- src/lib/shopify/index.ts
+++ src/lib/shopify/index.ts
@@ -34,9 +34,9 @@ import {
   ShopifyMenuOperation,
 } from "./types";
 import { getMenuQuery } from "./queries/menu";
-import { NextRequest, NextResponse } from "next/server";
-import { headers } from "next/headers";
-import { revalidateTag } from "next/cache";
+// import { NextRequest, NextResponse } from "next/server";
+// import { headers } from "next/headers";
+// import { revalidateTag } from "next/cache";
 
 const domain = process.env.SHOPIFY_STORE_DOMAIN
   ? ensureStartsWith(process.env.SHOPIFY_STORE_DOMAIN, "https://")
@@ -395,42 +395,42 @@ export async function addToCart(
 }
 
 
-export async function revalidate(req: NextRequest): Promise<NextResponse> {
-  // We always need to respond with a 200 status code to Shopify,
-  // otherwise it will continue to retry the request.
+// export async function revalidate(req: NextRequest): Promise<NextResponse> {
+//   // We always need to respond with a 200 status code to Shopify,
+//   // otherwise it will continue to retry the request.
 
-  const collectionWebhooks = [
-    "collections/create",
-    "collections/delete",
-    "collections/update",
-  ];
-  const productWebhooks = [
-    "products/create",
-    "products/delete",
-    "products/update",
-  ];
-  const topic = (await headers()).get("x-shopify-topic") || "unknown";
-  const secret = req.nextUrl.searchParams.get("secret");
-  const isCollectionUpdate = collectionWebhooks.includes(topic);
-  const isProductUpdate = productWebhooks.includes(topic);
+//   const collectionWebhooks = [
+//     "collections/create",
+//     "collections/delete",
+//     "collections/update",
+//   ];
+//   const productWebhooks = [
+//     "products/create",
+//     "products/delete",
+//     "products/update",
+//   ];
+//   const topic = (await headers()).get("x-shopify-topic") || "unknown";
+//   const secret = req.nextUrl.searchParams.get("secret");
+//   const isCollectionUpdate = collectionWebhooks.includes(topic);
+//   const isProductUpdate = productWebhooks.includes(topic);
 
-  if (!secret || secret !== process.env.SHOPIFY_REVALIDATION_SECRET) {
-    console.error("Invalid revalidation secret.");
-    return NextResponse.json({ status: 200 });
-  }
+//   if (!secret || secret !== process.env.SHOPIFY_REVALIDATION_SECRET) {
+//     console.error("Invalid revalidation secret.");
+//     return NextResponse.json({ status: 200 });
+//   }
 
-  if (!isCollectionUpdate && !isProductUpdate) {
-    // We don't need to revalidate anything for any other topics.
-    return NextResponse.json({ status: 200 });
-  }
+//   if (!isCollectionUpdate && !isProductUpdate) {
+//     // We don't need to revalidate anything for any other topics.
+//     return NextResponse.json({ status: 200 });
+//   }
 
-  if (isCollectionUpdate) {
-    revalidateTag(TAGS.collections);
-  }
+//   if (isCollectionUpdate) {
+//     revalidateTag(TAGS.collections);
+//   }
 
-  if (isProductUpdate) {
-    revalidateTag(TAGS.products);
-  }
+//   if (isProductUpdate) {
+//     revalidateTag(TAGS.products);
+//   }
 
-  return NextResponse.json({ status: 200, revalidated: true, now: Date.now() });
-}
+//   return NextResponse.json({ status: 200, revalidated: true, now: Date.now() });
+// }
