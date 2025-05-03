bf3649a2b14937a63c09de53227fdea7dee933f7
skupta <tosya2001@gmail.com>
2025-05-03 17:58:14 +0300

    getProduct query

--- src/lib/shopify/queries/product.ts
+++ src/lib/shopify/queries/product.ts
@@ -1,26 +1,22 @@
-import productFragment from "../fragments/product";
-
-export const getProductsQuery = /* GraphQL */ `
-  query getProducts(
-    $sortKey: ProductSortKeys
-    $reverse: Boolean
-    $query: String
-  ) {
-    products(sortKey: $sortKey, reverse: $reverse, query: $query, first: 50) {
-      edges {
-        node {
-          ...product
-        }
-      }
-    }
-  }
-  ${productFragment}
-`;
+import productFragment from '../fragments/product';
 
 export const getProductQuery = /* GraphQL */ `
   query getProduct($handle: String!) {
     product(handle: $handle) {
       ...product
+    }
+  }
+  ${productFragment}
+`;
+
+export const getProductsQuery = /* GraphQL */ `
+  query getProducts($sortKey: ProductSortKeys, $reverse: Boolean, $query: String) {
+    products(sortKey: $sortKey, reverse: $reverse, query: $query, first: 100) {
+      edges {
+        node {
+          ...product
+        }
+      }
     }
   }
   ${productFragment}
