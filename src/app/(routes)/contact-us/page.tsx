03122e6f781141df866747cd3af0fe14c6128020
skupta <tosya2001@gmail.com>
2025-05-08 00:38:28 +0300

    --

--- src/app/(routes)/contact-us/page.tsx
+++ src/app/(routes)/contact-us/page.tsx
@@ -1,23 +1,22 @@
 "use client";
 
 import { Container } from "@/components";
-import { z } from "zod";
 import styles from "@/styles";
 
-const FormSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-  email: z.string().email({
-    message: "Invalid email address.",
-  }),
-  phone: z.string().regex(/^\+?[0-9\s\-]{7,15}$/, {
-    message: "Invalid phone number.",
-  }),
-  message: z.string().min(10, {
-    message: "Message must be at least 10 characters.",
-  }),
-});
+// const FormSchema = z.object({
+//   username: z.string().min(2, {
+//     message: "Username must be at least 2 characters.",
+//   }),
+//   email: z.string().email({
+//     message: "Invalid email address.",
+//   }),
+//   phone: z.string().regex(/^\+?[0-9\s\-]{7,15}$/, {
+//     message: "Invalid phone number.",
+//   }),
+//   message: z.string().min(10, {
+//     message: "Message must be at least 10 characters.",
+//   }),
+// });
 
 export default function ContactUs() {
   return (
